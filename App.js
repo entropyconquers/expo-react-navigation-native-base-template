bbec92bf0401d6d5f67e1c265d1114d8354ea2a9
entropyconquers <breakingcortana5@gmail.com>
2023-03-06 23:37:07 +0530

    Init commit

--- App.js
+++ App.js
@@ -1,20 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// In App.js in a new project
+import { NativeBaseProvider, Text } from "native-base";
+import * as React from 'react';
+import { useCallback } from 'react';
+import { View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 
-export default function App() {
+SplashScreen.preventAutoHideAsync();
+
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
   );
 }
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push("Details")}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+const Stack = createNativeStackNavigator();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function App() {
+  const [fontsLoaded] = useFonts({
+    'Hellix-Bold': require('./assets/fonts/Hellix-Bold.ttf'),
+    'Hellix-Light': require('./assets/fonts/Hellix-Light.ttf'),
+    'Hellix-Medium': require('./assets/fonts/Hellix-Medium.ttf'),
+    'Hellix-Regular': require('./assets/fonts/Hellix-Regular.ttf'),
+    'Hellix-SemiBold': require('./assets/fonts/Hellix-SemiBold.ttf'),
+    'Hellix-ExtraBold': require('./assets/fonts/Hellix-ExtraBold.ttf'),
+    'Hellix-Black': require('./assets/fonts/Hellix-Black.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+  return (
+    <NativeBaseProvider onLayout={onLayoutRootView}>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            //hide header title
+            headerTitle: "",
+            //back button color black
+            headerTintColor: "black",
+            //backgrond color transparent
+            headerTransparent: true,
+          }}
+        >
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Details" component={DetailsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
+  );
+}
+
+export default App;
+
+
